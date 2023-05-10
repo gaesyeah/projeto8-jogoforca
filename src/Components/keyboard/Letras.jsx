176335b1361b4f625e922ad1370e174375f6a5d9
Gabriel Goes Santos Rios <gabrielgoesrios@gmail.com>
2023-05-10 15:30:23 -0300

    utilização de desestruturação do props.resetKey, para seguir a padronização do projeto, e melhora na semantica do ternatio que renderiza a palavra na tela

--- src/Components/keyboard/Letras.jsx
+++ src/Components/keyboard/Letras.jsx
@@ -2,11 +2,13 @@ import Letra from "./Letra";
 
 function Letras(props) {
 
+    const {resetKey} = props;
+
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
     return (
         <div className="keyBoard" key={'teste'}>
-            {alfabeto.map(letra => <Letra letra={letra} parentProps={props} key={`${letra} (${props.resetKey})`}/>)}
+            {alfabeto.map(letra => <Letra letra={letra} parentProps={props} key={`${letra} (${resetKey})`}/>)}
         </div>
     );
 }
